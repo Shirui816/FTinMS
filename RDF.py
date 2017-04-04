0b445eb80dbde254aa20d3c5b89b31fd37a53557
施呵呵 <shirui816@gmail.com>
2017-04-04 13:27:05 -0500

    Update RDF.py

--- RDF.py
+++ RDF.py
@@ -1,16 +1,11 @@
 from sys import argv
 import pandas as pd
 import numpy as np
-from functions import crdf
 import time
 import accelerate as acc
-import matplotlib
-from matplotlib import pyplot as plt
 fn = argv[1]
 print('Box origin must be at the center!')
 pos = pd.read_csv(fn, delim_whitespace=True, squeeze=1, header=None).values
-import time
-import numpy as np
 
 Ndim = 500 # Finess of delta function
 V = box[0]*box[1]*box[2]
@@ -29,13 +24,13 @@ RDF = np.fft.ifftn(FP).real # IFFT{<rho(K)rho(-K)>}, 1/N\sum_i......(see numpy.f
 print('IFFT time: %s' % (time.time()-s))
 RDF[0,0,0] -= pos.shape[0]
 RDF = np.fft.fftshift(RDF)
-rbin = 0.2 # >= box / Ndiv
+rbin = 0.2 # histogram for g(\bm{r}) to g(r)
 rx = e[0][:Ndim] + 0.5*(e[0][-1]-e[0][-2])
 ry = e[1][:Ndim] + 0.5*(e[1][-1]-e[1][-2])
 rz = e[2][:Ndim] + 0.5*(e[2][-1]-e[2][-2])
-from numba import jit
 
-@jit # normalize g(R) to g(r)
+from numba import jit
+@jit # histogram g(R) to g(r)
 def norm_r(RDF, rbin, rx, ry, rz):
     rdf = np.zeros(int(box.max()/2*3**0.5/rbin)+1, dtype=np.float)
     cter = np.zeros(rdf.shape, dtype=np.float)
