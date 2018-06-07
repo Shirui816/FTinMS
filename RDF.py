e9915150eb1b4ed8c5fbce7e08d20588e761dc9f
施呵呵 <shirui816@gmail.com>
2018-06-07 12:04:49 +0800

    Update RDF.py

--- RDF.py
+++ RDF.py
@@ -1,30 +1,37 @@
 from sys import argv
 import pandas as pd
 import numpy as np
-import accelerate as acc
+from numba import jit
+
 fn = argv[1]
-print('Box origin must be at the center!')
-# This `centerage` is necessary for r matrix
+box = np.array([50, 50, 50])
 pos = pd.read_csv(fn, delim_whitespace=True, squeeze=1, header=None).values
 
-Ndim = 500 # Finess of delta function
-V = box[0]*box[1]*box[2]
-rho = pos.shape[0]/V
-p, e = np.histogramdd(pos, bins=(Ndim, Ndim, Ndim), range=((-box[0]/2, box[0]/2), (-box[1]/2, box[1]/2),(-box[2]/2, box[2]/2)))
-p = np.fft.fftshift(p) # POS is of center-origin, here move origin to cornor.
-fp = acc.mkl.fftpack.fftn(p) # Accelerate package, strange, np.fft is actually faster than acc.mkl.fftpack.fft for N-dimensional ffts
+Ndim = 500  # Finess of delta function
+p, e = np.histogramdd(pos, bins=(Ndim, Ndim, Ndim),
+                      range=((-box[0]/2, box[0]/2), (-box[1]/2, box[1]/2),
+                      (-box[2]/2, box[2]/2)))
+fp = np.fft.rfftn(p)
+# rfft for efficiency.
+# Accelerate package, strange,
+# np.fft is actually faster than accelerate.mkl.fftpack.fft
+# for N-dimensional ffts in anaconda
 FP = fp*fp.conj()
-RDF = np.fft.ifftn(FP).real # IFFT{<rho(K)rho(-K)>}, 1/N\sum_i......(see numpy.fft, so N_bins is needed)
-RDF[0,0,0] -= pos.shape[0] # g(\bm{r}) = IFFT{<rho(K)rho(-K)>} - N\delta(\bm{r}) for gAA cases.
+RDF = np.fft.irfftn(FP).real
+# IFFT{<rho(K)rho(-K)>}, 1/N\sum_i......(see numpy.fft, so N_bins is needed)
+RDF[0, 0, 0] -= pos.shape[0]
+# g(\bm{r}) = IFFT{<rho(K)rho(-K)>} - N\delta(\bm{r}) for gAA cases.
 RDF = np.fft.fftshift(RDF)
-rbin = 0.2 # histogram for g(\bm{r}) to g(r)
+# Move to box center for e is in -box/2 box/2
+rbin = 0.2  # histogram for g(\bm{r}) to g(r)
 rx = e[0][:Ndim] + 0.5*(e[0][-1]-e[0][-2])
 ry = e[1][:Ndim] + 0.5*(e[1][-1]-e[1][-2])
 rz = e[2][:Ndim] + 0.5*(e[2][-1]-e[2][-2])
 
-from numba import jit
-@jit # histogram g(R) to g(r)
+
+@jit  # histogram g(R) to g(r)
 def norm_r(RDF, rbin, rx, ry, rz):
+    r"""Add up $gr(x,y,z) -> g(\sqrt{x^2+y^2+z^2})$."""
     rdf = np.zeros(int(box.max()/2*3**0.5/rbin)+1, dtype=np.float)
     cter = np.zeros(rdf.shape, dtype=np.float)
     for i in range(Ndim):
@@ -32,14 +39,14 @@ def norm_r(RDF, rbin, rx, ry, rz):
             for k in range(Ndim):
                 rr = rx[i]**2+ry[j]**2+rz[k]**2
                 r = int(rr**0.5/rbin)
-                rdf[r] += RDF[i,j,k]
+                rdf[r] += RDF[i, j, k]
                 cter[r] += 1
     return np.nan_to_num(rdf/cter)
 
 
-rdf = norm_r(RDF, rbin, rx,ry,rz)
-rdf /= pos.shape[0] * rho / V # NA*NB/V^2 for gAB(r)
-rdf *= Ndim**3 # 1/N problem induced by FFT
+rdf = norm_r(RDF, rbin, rx, ry, rz)
+rdf /= pos.shape[0] * pos.shape[0]  # NA*NB for gAB(r)
+rdf *= Ndim**3  # 1/N problem induced by FFT
 rr = np.arange(rdf.shape[0])*rbin
 
 o = open('rdf.txt', 'w')
