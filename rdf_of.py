622f6e4378a3b4eb8080c8534300718ac346ea85
Shirui <2361394+Shirui816@users.noreply.github.com>
2019-06-21 15:04:19 +0800

    bug fix.

--- rdf_of.py
+++ rdf_of.py
@@ -82,49 +82,78 @@ def cu_cell_list(pos, box, ibox, gpu=0):
         cu_cell_ind[bpg, tpb](pos, box, ibox, cell_id)
     cell_list = np.argsort(cell_id)  # pyculib radixsort for cuda acceleration.
     cell_id = cell_id[cell_list]
-    cell_counts = np.r_[0, np.cumsum(np.bincount(cell_id, minlength=n_cell + 1))]
+    cell_counts = np.r_[0, np.cumsum(np.bincount(cell_id, minlength=n_cell))]
     return cell_list.astype(np.int64), cell_counts.astype(np.int64)
 
 
-@cuda.jit(
-    "void(float64[:,:],float64[:,:],float64[:],int64[:],float64[:],"
-    "float64[:],float64,float64,int64[:],int64[:],int64[:,:],int64[:])"
-)
-def rdf_of_ab(a, b, box, ibox, da, db, bs, rc, cl, cc, ret, dim):
-    r"""
-    :param a: positions of a, (n_pa, n_d)
-    :param b: positions of b, (n_pb, n_d)
-    :param box: box, (n_d,)
-    :param ibox: bins, (n_d,)
-    :param da: diameters of a, (n_pa,)
-    :param db: diameters of b, (n_pb,)
-    :param bs: binsize of rdf, double
-    :param rc: r_cut of rdf, double
-    :param cl: cell-list of b, (n_pb,)
-    :param cc: cell-count-cum, (n_cell + 1,)
-    :param ret: rdfs of (n_pa, n_bin)
-    :param dim: dimension of (-1, 0, 1) vector in calculating adjacent cells
-    : a 3-D case would be (3, 3, 3), simply (3,) * n_d
-    :return: None
-    """
-    i = cuda.grid(1)
-    if i >= a.shape[0]:
-        return
-    cell_i = cu_cell_id(a[i], box, ibox)  # a[i] in which cell
-    cell_vec_i = cuda.local.array(3, nb.int64)  # unravel the cell id
-    unravel_index_f_cu(cell_i, ibox, cell_vec_i)  # unravel the cell id
-    cell_vec_j = cuda.local.array(3, nb.int64)
-    for j in range(a.shape[1] ** 3):
-        unravel_index_f_cu(j, dim, cell_vec_j)  # -1, -1, -1 to +1, +1, +1
-        _add_local_arr_mois_1(cell_vec_j, cell_vec_i)
-        cell_j = ravel_index_f_cu(cell_vec_j, ibox)  # ravel cell id vector to cell id
-        start = cc[cell_j]  # start pid in the cell_j th cell
-        end = cc[cell_j + 1]  # end pid in the cell_j th cell
-        for k in range(start, end):  # particle ids in cell_j
-            pid_k = cl[k]
-            dij = pbc_dist_cu(a[i], b[pid_k], box) - (da[i] + db[pid_k]) / 2 + 1
-            if dij < rc:
-                cuda.atomic.add(ret[i], int(dij / bs), 1)
+def rdf_of_ab(a, b, box, da, db, bs, rc, gpu=0):
+    ret = np.zeros((a.shape[0], int(rc / bs)), dtype=np.int64)
+    dim = np.ones(a.shape[1], dtype=np.int64) * 3
+    ndim = a.shape[1]
+    ibox = np.asarray(np.round(_box / _rc), dtype=np.int64)
+    cl, cc = cu_cell_list(b, box, ibox, gpu=gpu)
+
+    # ndim: dimentsion, a.shape[1], jit does not support
+    # using a.shape[1] directly in cuda.local.array creation,
+    # numba==0.44.1
+    # dim = np.ones(ndim, dtype=np.int64) * ndim
+    # dimension of (-1, 0, 1) vector in calculating adjacent cells
+    # a 3-D case would be (3, 3, 3), simply (3,) * n_d
+    # _dim is required for in nonpython mode, sig=array(int64, 1d, A)
+    # and using dim directly would by `readonly array(int64, 1d, C)'
+
+    @cuda.jit(
+        "void(float64[:,:],float64[:,:],float64[:],int64[:],float64[:],"
+        "float64[:],float64,float64,int64[:],int64[:],int64[:,:], int64[:])"
+    )
+    def _rdf(_a, _b, _box, _ibox, _da, _db, _bs, _rc, _cl, _cc, _ret, _dim):
+        r"""
+        :param _a: positions of a, (n_pa, n_d)
+        :param _b: positions of b, (n_pb, n_d)
+        :param _box: box, (n_d,)
+        :param _ibox: bins, (n_d,)
+        :param _da: diameters of a, (n_pa,)
+        :param _db: diameters of b, (n_pb,)
+        :param _bs: binsize of rdf, double
+        :param _rc: r_cut of rdf, double
+        :param _cl: cell-list of b, (n_pb,)
+        :param _cc: cell-count-cum, (n_cell + 1,)
+        :param _ret: rdfs of (n_pa, n_bin)
+        :return: None
+        """
+        i = cuda.grid(1)
+        if i >= _a.shape[0]:
+            return
+        cell_i = cu_cell_id(_a[i], _box, _ibox)  # a[i] in which cell
+        cell_vec_i = cuda.local.array(ndim, nb.int64)  # unravel the cell id
+        unravel_index_f_cu(cell_i, _ibox, cell_vec_i)  # unravel the cell id
+        cell_vec_j = cuda.local.array(ndim, nb.int64)
+        for j in range(_a.shape[1] ** 3):
+            unravel_index_f_cu(j, _dim, cell_vec_j)
+            _add_local_arr_mois_1(cell_vec_j, cell_vec_i)
+            # cell_vec_i + (-1, -1, -1) to (+1, +1, +1)
+            # unraveled results would be (0,0,0) to (2,2,2) for dim=3
+            cell_j = ravel_index_f_cu(cell_vec_j, _ibox)  # ravel cell id vector to cell id
+            start = _cc[cell_j]  # start pid in the cell_j th cell
+            end = _cc[cell_j + 1]  # end pid in the cell_j th cell
+            for k in range(start, end):  # particle ids in cell_j
+                pid_k = _cl[k]
+                dij = pbc_dist_cu(_a[i], _b[pid_k], _box) - (_da[i] + _db[pid_k]) / 2 + 1
+                if dij < rc:
+                    cuda.atomic.add(_ret[i], int(dij / _bs), 1)
+    with cuda.gpus[0]:
+        device = cuda.get_current_device()
+        tpb = device.WARP_SIZE
+        bpg = ceil(a.shape[0] / tpb)
+        _rdf[bpg, tpb](
+            a, b, box, ibox, da, db, bs, rc, cl, cc, ret, dim
+        )
+    rho_b = b.shape[0] / np.multiply.reduce(box)
+    r = (np.arange(ret.shape[1] + 1) + 0.5) * bs
+    dV = 4 / 3 * np.pi * np.diff(r ** 3)
+    ret = ret / np.expand_dims(dV, 0) / rho_b
+    np.savetxt('rdf.txt', np.vstack([r[:-1], ret.mean(axis=0)]).T, fmt='%.6f')
+    return r, ret
 
 
 if __name__ == "__main__":
@@ -137,16 +166,4 @@ if __name__ == "__main__":
     _da = np.ones(_a.shape[0], dtype=np.float)
     _db = np.ones(_b.shape[0], dtype=np.float)
     _bs = 0.1
-    _cl, _cc = cu_cell_list(_b, _box, _ibox, gpu=0)
-    _ret = np.zeros((_a.shape[0], int(_rc / _bs)), dtype=np.int64)
-    _dim = np.ones(_a.shape[1], dtype=np.int64) * 3
-    with cuda.gpus[0]:
-        _device = cuda.get_current_device()
-        _tpb = _device.WARP_SIZE
-        _bpg = ceil(_a.shape[0] / _tpb)
-        rdf_of_ab[_bpg, _tpb](_a, _b, _box, _ibox, _da, _db, _bs, _rc, _cl, _cc, _ret, _dim)
-    _rho_b = _b.shape[0] / np.multiply.reduce(_box)
-    _r = (np.arange(_ret.shape[1] + 1) + 0.5) * _bs
-    _dV = 4 / 3 * np.pi * np.diff(_r ** 3)
-    _ret = _ret / np.expand_dims(_dV, 0) / _rho_b
-    np.savetxt('rdf.txt', np.vstack([_r[:-1], _ret.mean(axis=0)]).T, fmt='%.6f')
+    _r, _ret = rdf_of_ab(_a, _b, _box, _da, _db, _bs, _rc, gpu=0)
