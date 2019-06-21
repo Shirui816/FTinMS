59166a5fd02d2ac98ed4ddd76d1ba2f3151c86b9
Shirui <2361394+Shirui816@users.noreply.github.com>
2019-06-21 13:38:38 +0800

    rdf with cuda and cell-list with r_cut

---
+++ rdf_of.py
@@ -0,0 +1,155 @@
+import numba as nb
+import numpy as np
+from math import ceil
+from math import floor
+from math import sqrt
+from numba import cuda
+
+
+@cuda.jit("int64(float64[:], float64[:], int64[:])", device=True)
+def cu_cell_id(p, box, ibox):  # In the Fortran way
+    ret = floor((p[0] / box[0] + 0.5) * ibox[0])
+    tmp = ibox[0]
+    for i in range(1, p.shape[0]):
+        ret += floor((p[i] / box[i] + 0.5) * ibox[i]) * tmp
+        tmp *= ibox[i]
+    return ret
+    # return floor((p[0] / box[0] + 0.5) * ibox[0]) + \
+    # floor((p[1] / box[1] + 0.5) * ibox[1]) * ibox[0] + \
+    # floor((p[2] / box[2] + 0.5) * ibox[2]) * ibox[1] * ibox[0]
+    # +0.5 for 0 is at center of box.
+    # unravel in Fortran way.
+
+
+@cuda.jit("void(float64[:, :], float64[:], int64[:], int64[:]")
+def cu_cell_ind(pos, box, ibox, ret):
+    i = cuda.grid(1)
+    if i < pos.shape[0]:
+        pi = pos[i]
+        ic = cu_cell_id(pi, box, ibox)
+        ret[i] = ic
+
+
+@cuda.jit('float64(float64[:], float64[:], float64[:])', device=True)
+def pbc_dist_cu(a, b, box):
+    tmp = 0
+    for i in range(a.shape[0]):
+        d = b[i] - a[i]
+        d = d - floor(d / box[i] + 0.5) * box[i]
+        tmp += d * d
+    return sqrt(tmp)
+
+
+@cuda.jit("void(int64, int64[:], int64[:])", device=True)
+def unravel_index_f_cu(i, dim, ret):  # unravel index in Fortran way.
+    for k in range(dim.shape[0]):
+        ret[k] = int(i % dim[k])
+        i = (i - ret[k]) / dim[k]
+
+
+@cuda.jit("int64(int64[:], int64[:])", device=True)
+def ravel_index_f_cu(i, dim):  # ravel index in Fortran way.
+    ret = (i[0] + dim[0]) % dim[0]
+    tmp = dim[0]
+    for k in range(1, dim.shape[0]):
+        ret += ((i[k] + dim[k]) % dim[k]) * tmp
+        tmp *= dim[k]
+    return ret
+
+
+def cell_count(cell_id):
+    r"""
+    :param cell_id: must be sorted.
+    :return: cumsum of count of particles in cells.
+    """
+    return np.append(0, np.cumsum(np.bincount(cell_id, minlength=cell_id.max() + 1)))
+
+
+@cuda.jit("void(int64[:], int64[:])")
+def cu_cell_count(cell_id, ret):
+    i = cuda.grid(1)
+    if i >= cell_id.shape[0]:
+        return
+    cuda.atomic.add(ret, cell_id[i] + 1, 1)
+
+
+def cu_cell_list(pos, box, ibox, gpu=0):
+    n = pos.shape[0]
+    cell_id = np.zeros(n).astype(np.int64)
+    with cuda.gpus[gpu]:
+        device = cuda.get_current_device()
+        tpb = device.WARP_SIZE
+        bpg = ceil(n / tpb)
+        cu_cell_ind[bpg, tpb](pos, box, ibox, cell_id)
+    cell_list = np.argsort(cell_id)  # pyculib radixsort for cuda acceleration.
+    cell_id = cell_id[cell_list]
+    cell_counts = np.r_[
+        0, np.diff(
+            np.flatnonzero(np.diff(cell_id)) + 1, prepend=0, append=cell_id.shape[0]
+        ).cumsum()
+    ]
+    return cell_list.astype(np.int64), cell_counts.astype(np.int64)
+
+
+@cuda.jit(
+    "void(float64[:,:],float64[:,:],float64[:],int64[:],float64[:],"
+    "float64[:],float64,float64,int64[:],int64[:],int64[:,:],int64[:]"
+)
+def rdf_of_ab(a, b, box, ibox, da, db, bs, rc, cl, cc, ret, dim):
+    r"""
+    :param a: positions of a, (n_pa, n_d)
+    :param b: positions of b, (n_pb, n_d)
+    :param box: box, (n_d,)
+    :param ibox: bins, (n_d,)
+    :param da: diameters of a, (n_pa,)
+    :param db: diameters of b, (n_pb,)
+    :param bs: binsize of rdf, double
+    :param rc: r_cut of rdf, double
+    :param cl: cell-list of b, (n_pb,)
+    :param cc: cell-count-cum, (n_cell + 1,)
+    :param ret: rdfs of (n_pa, n_bin)
+    :param dim: dimension of (-1, 0, 1) vector in calculating adjacent cells
+    : a 3-D case would be (3, 3, 3), simply (3,) * n_d
+    :return: None
+    """
+    i = cuda.grid(1)
+    if i >= a.shape[0]:
+        return
+    cell_i = cu_cell_id(a[i], box, ibox)  # a[i] in which cell
+    cell_vec_i = cuda.local.array(a.shape[1], nb.int64)  # unravel the cell id
+    unravel_index_f_cu(cell_i, ibox, cell_vec_i)  # unravel the cell id
+    cell_vec_j = cuda.local.array(a.shape[1], nb.int64)
+    for j in range(a.shape[1] ** 3):
+        unravel_index_f_cu(j, dim, cell_vec_j)  # -1, -1, -1 to +1, +1, +1
+        cell_vec_j = cell_vec_i + cell_vec_j - 1
+        cell_j = ravel_index_f_cu(cell_vec_j, ibox)  # ravel cell id vector to cell id
+        start = cc[cell_j]  # start pid in the cell_j th cell
+        end = cc[cell_j + 1]  # end pid in the cell_j th cell
+        for k in range(start, end):  # particle ids in cell_j
+            pid_k = cl[k]
+            dij = pbc_dist_cu(a[i], b[pid_k], box) - (da[i] + db[pid_k]) / 2 + 1
+            if dij < rc:
+                cuda.atomic.add(ret[i], int(dij / bs), 1)
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    _a, _b = np.loadtxt(argv[1], dtype=np.float), np.loadtxt(argv[2], dtype=np.float)
+    _box = np.array([50, 50, 50], dtype=np.float)
+    _rc = 3.0
+    _ibox = np.asarray(np.round(_box / _rc), dtype=np.int64)
+    _da = np.ones(_a.shape[0], dtype=np.float)
+    _db = np.ones(_b.shape[0], dtype=np.float)
+    _bs = 0.1
+    _cl, _cc = cu_cell_list(_b, _box, _ibox, gpu=0)
+    _ret = np.zeros((_a.shape[0], int(_rc / _bs)), dtype=np.int64)
+    _dim = np.ones(_a.shape[1], dtype=np.int64) * 3
+    with cuda.gpus[0]:
+        _device = cuda.get_current_device()
+        _tpb = _device.WARP_SIZE
+        _bpg = ceil(_a.shape[0] / _tpb)
+        rdf_of_ab[_bpg, _tpb](_a, _b, _box, _ibox, _da, _db, _bs, _rc, _cl, _cc, _ret, _dim)
+    _r = (np.arange(_ret.shape[0] + 1) + 0.5) * _bs
+    _dV = 4 / 3 * np.pi * np.diff(_r ** 3)
+    _ret = _ret / _dV
