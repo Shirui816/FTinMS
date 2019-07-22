7d1201dcb7123242846248562f0ac9736a72347b
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2019-07-22 23:23:11 +0800

    reformat

--- rdf_cython_parallel_cell_list/setup.py
+++ rdf_cython_parallel_cell_list/setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-from Cython.Distutils import build_ext
 import numpy as np
+from Cython.Distutils import build_ext
 
 ext_modules = [
     Extension("func",
@@ -16,6 +16,6 @@ ext_modules = [
 setup(
     name="func",
     cmdclass={"build_ext": build_ext},
-    include_dirs = [np.get_include()],
+    include_dirs=[np.get_include()],
     ext_modules=ext_modules
 )
