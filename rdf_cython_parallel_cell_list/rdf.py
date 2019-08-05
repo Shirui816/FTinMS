4b8a1ea343d133d4706bbe2e3433d971e3f7cd2e
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2019-08-05 22:47:08 +0800

    Update rdf.py

--- rdf_cython_parallel_cell_list/rdf.py
+++ rdf_cython_parallel_cell_list/rdf.py
@@ -1,18 +1,27 @@
-from func import rdf, linked_cl
 import numpy as np
+import func
+
 
 if __name__ == "__main__":
-    import sys
-    _a, _b = np.loadtxt(sys.argv[1], dtype=np.float), np.loadtxt(sys.argv[2], dtype=np.float)
+    from sys import argv
+
+    _a, _b = np.loadtxt(argv[1], dtype=np.float), np.loadtxt(argv[2], dtype=np.float)
     _box = np.array([50, 50, 50], dtype=np.float)
-    _rc = 3.0
-    _ibox = np.asarray(np.round(_box / _rc), dtype=np.int64)
+    _rc = 2.0
+    _ibox = np.asarray((_box / _rc), dtype=np.int64)
+    #print(_ibox)
     _bs = 0.1
-    _nb = int(_rc / _bs)
-    _head, _body = linked_cl(_b, _box, _ibox)
-    print('done')
-    _ret = rdf(_a, _b, _box, _head, _body, _ibox, _bs, _nb)
+    _nb = int(_rc/_bs)
+    #_head, _body = linked_cl(_b, _box, _ibox)
+    #print('done')
+    import time
+    s = time.time()
+    _ret = func.rdf(_a, _b, _box, _bs, _nb)
+    print(round(time.time()-s,3))
     _rho = _b.shape[0] / np.multiply.reduce(_box)
-    _r = (np.arange(_ret.shape[1] + 1) + 0.5) * _bs
+    _r = (np.arange(_ret.shape[0] + 1) + 0.5) * _bs
     _dV = 4 / 3 * np.pi * np.diff(_r ** 3)
-    np.savetxt('rdf.txt', np.vstack([_r[:-1], _ret.mean(axis=0) / _dV / _rho]).T, fmt='%.6f')
+    #np.savetxt('heqd.txt', _head, fmt='%d')
+    #np.savetxt('body.txt', _body, fmt='%d')
+    #np.savetxt('rdf.txt', np.vstack([_r[:-1], _ret.sum(axis=0)]).T, fmt='%.6f')#.mean(axis=0)/_dV / _rho]).T, fmt='%.6f')
+    np.savetxt('rdf_dv.txt', np.vstack([_r[:-1], _ret/_a.shape[0]/_dV / _rho]).T, fmt='%.6f')
