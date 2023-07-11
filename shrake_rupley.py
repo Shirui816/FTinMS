6156e27308e53b0814f015738306aadc83d31bf4
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2023-07-11 19:51:42 +0800

    Update shrake_rupley.py

--- shrake_rupley.py
+++ shrake_rupley.py
@@ -26,27 +26,36 @@ def pbc(r, d):
     return r - d * np.rint(r / d)
 
 
-def shrake_rupley_sasa(x, radiivdw, probe_radius=1.4, n_points=960, box=None):
+def shrake_rupley_sasa(x: np.ndarray[np.float64, 2], radii_vdw: np.ndarray[np.float64, 1],
+                       probe_radius: float = 1.4, n_points: int = 960,
+                       box: Union[None, np.ndarray[np.float64, 2]] = None) -> np.ndarray[np.float64, 1]:
+    r"""
+    :param x: coordinates.
+    :param radii_vdw: vdw radius of atoms
+    :param probe_radius: probe molecule radius, 1.4 angstrom for water
+    :param n_points: mesh size for sphere
+    :param box: box size for pbc systems, box=None (default) is for no pbc
+    :return: array of solvent accessible surface area for atoms
+    """
     sph = fibonacci_sphere(n_points)
-    radiivdw = radiivdw + probe_radius
-    cut_off = radiivdw.max() * 2.
+    radii_vdw = radii_vdw + probe_radius
+    cut_off = radii_vdw.max() * 2.
     asa_ary = np.zeros((len(x)), dtype=np.int64)
     if box is None:
         box = np.ones(3) * (x.max(axis=0) - x.min(axis=0) + 2 * cut_off)
     x = np.mod(x, box)
     kdt = cKDTree(x, boxsize=box)
     for i in range(len(x)):
-        r_i = radiivdw[i]
+        r_i = radii_vdw[i]
         s_on_i = np.mod(sph * r_i + x[i], box)
         avail_sph = np.ones(n_points)
         kdt_sph = cKDTree(s_on_i, boxsize=box)
         for j in kdt.query_ball_point(x[i], cut_off):
             if i != j:
                 dij = np.linalg.norm(pbc(x[i] - x[j], box), axis=-1)
-                if dij < r_i + radiivdw[j]:
-                    avail_sph[kdt_sph.query_ball_point(x[j], radiivdw[j])] = 0
+                if dij < r_i + radii_vdw[j]:
+                    avail_sph[kdt_sph.query_ball_point(x[j], radii_vdw[j])] = 0
         asa_ary[i] = avail_sph.sum()
-    f = 4. * np.pi * radiivdw ** 2. / n_points
+    f = 4. * np.pi * radii_vdw ** 2. / n_points
     asa_ary = asa_ary * f
     return asa_ary
-  
